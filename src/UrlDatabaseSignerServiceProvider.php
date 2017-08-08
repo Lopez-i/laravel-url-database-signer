618ddf18ccb93fc3ab2739416bd64553c23091a5
Loïc Lopez <loic.lopez@epitech.eu>
2017-08-08 22:24:03 +0200

    oops

--- src/UrlDatabaseSignerServiceProvider.php
+++ src/UrlDatabaseSignerServiceProvider.php
@@ -19,15 +19,15 @@ class UrlDatabaseSignerServiceProvider extends ServiceProvider
             $source_middleware = __DIR__.'/Middleware/ValidateUrlSignature.php';
             $this->publishes(
                 [
-                    $source_config => $this->app->config_path('url-database-signer.php'),
+                    $source_config => config_path('url-database-signer.php'),
                 ]);
             $this->publishes(
                 [
-                    $source_db => $this->app->database_path('2017_07_22_144134_create_signed_urls_table.php'),
+                    $source_db => database_path('2017_07_22_144134_create_signed_urls_table.php'),
                 ]);
             $this->publishes(
                 [
-                    $source_middleware => $this->app->base_path('./app/Http/Middleware')
+                    $source_middleware => base_path('./app/Http/Middleware')
                 ]);
             $this->mergeConfigFrom($source_config, 'url-database-signer.php');
         }
